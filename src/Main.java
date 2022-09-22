44dd7b61712c715be91ec42cd4d1cc754a19fe29
Johan M <cph-jm390@cphbusiness.dk>
2022-09-22 11:59:30 +0200

    main framework

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 import Entitet.Bog;
 import MineUtil.BrugerInput;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
+        List<Bog> bogList = null;
 
+        while (true) {
+            switch (BrugerInput.getString("hvad ønsker du")) {
+                case "poggers":
+                    break;
+                case "gamers":
+                    break;
+                case "slackergruppen":
+                    break;
+
+
+
+
+            }
+
+
+        }
     }
 }
