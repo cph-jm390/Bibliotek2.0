7cbbc2850ec05a17a55e91e65c3dc96f7ba9dada
Johan M <cph-jm390@cphbusiness.dk>
2022-09-22 12:31:19 +0200

    Dialog create

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,11 @@
-import Entitet.Bog;
-import MineUtil.BrugerInput;
+import MineUtil.Dialog;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        List<Bog> bogList = null;
-
-        while (true) {
-            switch (BrugerInput.getString("hvad ønsker du")) {
-                case "poggers":
-                    break;
-                case "gamers":
-                    break;
-                case "vis kartotek":
-                    udskriv(bogList);
-
-
-            }
-
-
-        }
-
-    }
-
-    public static void udskriv(List<Bog> bøger) {
-
-        for (Bog bog : bøger) {
-
-            System.out.println(bog.toString());
-        }
+        Dialog.terminalDialog();
 
     }
 }
